1cbcd3b3cf511bded7e68c3593e2183b2be0e31f
AndPrim <andreyminoss@gmail.com>
2023-11-08 19:42:00 +0500

    TwoCommitLocal

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import ru.netology.statistic.StatisticsService;
+
+public class Main {
+    public static void main(String[] args) {
+        StatisticsService service = new StatisticsService();
+    }
+}
